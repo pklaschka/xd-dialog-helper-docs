48e937ad28034555245d02b379cf5f37ce533195
Pablo Klaschka <contact@pabloklaschka.de>
2019-08-21 14:39:44 +0200

    Prepared for publishing

---
+++ website/static/editor/precache-manifest.401c1ac932dc25459e027b69989285d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c5cff6ff929ccbe9eb836ee6ba86f96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9a4d34e1c19b8ec2fe7",
+    "url": "/static/css/main.15ff6040.chunk.css"
+  },
+  {
+    "revision": "df89c6452ef1e32744c8",
+    "url": "/static/js/2.7ca4a58e.chunk.js"
+  },
+  {
+    "revision": "e9a4d34e1c19b8ec2fe7",
+    "url": "/static/js/main.25e3b8e9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "18a5bc6056f5260ef0d9860df01595d5",
+    "url": "/static/media/logo.18a5bc60.png"
+  },
+  {
+    "revision": "9e38858712dbdab5cae87411656db447",
+    "url": "/static/media/spectrum-css-icons-medium.9e388587.svg"
+  }
+]);
